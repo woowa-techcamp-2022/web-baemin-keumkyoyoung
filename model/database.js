2204beceb2e14ab80b3681cbed5ec738a0e2f1d6
youngkyo0504 <keumky2@gmail.com>
2022-07-08 10:11:35 +0900

    fix deploy

--- model/database.js
+++ model/database.js
@@ -1,6 +1,6 @@
 const { Level } = require("level");
 
-const db = new Level("database/user", { valueEncoding: "json" });
+const db = new Level("lev/user", { valueEncoding: "json" });
 
 const user = db.sublevel("user", { valueEncoding: "json" });
 const session = db.sublevel("session", { valueEncoding: "json" });
