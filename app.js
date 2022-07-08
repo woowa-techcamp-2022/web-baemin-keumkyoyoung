ed66ab833949903c83ba7e821785c462b0d3bf08
youngkyo0504 <keumky2@gmail.com>
2022-07-08 10:22:27 +0900

    fix: deploy bug

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require("morgan");
 var app = express();
 
 var clientRouter = require("./routes/client");
-var apiRouter = require("./routes/api/index");
+var apiRouter = require("./routes/api");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var usersRouter = require("./users");
-var loginRouter = require("./login");
-
-router.use("/user", usersRouter);
-router.use("/login", loginRouter);
-
-module.exports = router;
